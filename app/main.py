66c237d48d0b61fe851f4e4b68f88ccca2150d6f
Razan Rafi Akmaluzzuhair <rafirazanakmaluzzuhair@gmail.com>
2026-04-30 18:32:47 +0700

    Petra Push

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 # CORS middleware — izinkan frontend mengakses API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://drive-tree-f.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
